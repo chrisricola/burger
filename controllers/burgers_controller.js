9438b6427052246e93eaf4027c2759b3c384c10a
Chris Rico <chris.r.rico@gmail.com>
2020-06-01 23:16:21 -0700

    created css and burgers js, filled out the controller, changed stuff on the layouts

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -32,8 +32,8 @@ router.put("/api/burger/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  burger.update({
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE burger_db;
 USE burger_db;
 
-CREATE TABLE burgers
+CREATE TABLE burger
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
