0ddb3e2a890071a511552f3fb46417b02d62914b
Chris Rico <chris.r.rico@gmail.com>
2020-06-01 23:39:14 -0700

    fixed the s in the seed sql and made changes to the assests file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ('Meeses');
-INSERT INTO burgers (burger_name) VALUES ('Bobbi');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burger (burger_name) VALUES ('Meeses');
+INSERT INTO burger (burger_name) VALUES ('Bobbi');
+INSERT INTO burger (burger_name, devoured) VALUES ('Sylvester', true);
